715ac77936a75f7b3efa912fdebdc53bc7bf2a35
Jason <jhughes3818@gmail.com>
2022-12-13 08:10:16 +1100

    fixed call of signin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {!session ? (
-        <button onClick={signIn}>Sign In</button>
+        <button onClick={() => signIn}>Sign In</button>
       ) : (
         <Layout>
           <BlockMain />
