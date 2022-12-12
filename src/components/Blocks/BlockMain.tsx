d5d45bf4ee9ce9a98ce4bc9b0b5a254e96487e7f
Jason <jhughes3818@gmail.com>
2022-12-12 19:52:38 +1100

    init

---
+++ src/components/Blocks/BlockMain.tsx
@@ -0,0 +1,8 @@
+export default function BlockMain() {
+  return (
+    <div className="h-36 w-96 rounded-md border border-gray-200 bg-white p-5 shadow-md">
+      <h1 className="text-2xl font-bold">Block Title</h1>
+      <p>Block Body</p>
+    </div>
+  );
+}
