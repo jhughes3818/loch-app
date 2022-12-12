46d802692dd31d1a335b6345cc4c09607d1d9b70
Jason <jhughes3818@gmail.com>
2022-12-13 08:28:49 +1100

    changed call of signin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
             <div className="mt-6 grid place-items-center">
               <button
                 className="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                onClick={() => signIn}
+                onClick={() => signIn()}
               >
                 Sign In
               </button>
